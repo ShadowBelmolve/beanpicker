ef3eb9fcc8bdd08271eb63f4fbc5d8c59d9c0c10
Renan Tomal Fernandes <renan@kauamanga.com.br>
2010-12-28 16:49:41 -0400

    version changed

--- lib/beanpicker/version.rb
+++ lib/beanpicker/version.rb
@@ -1,7 +1,7 @@
 module Beanpicker
 
   # A array with the version [MAJOR, MINOR, PATCH]
-  VERSION = [0, 0, 1]
+  VERSION = [0, 1, 0]
 
   # A string of the array joined by "."
   VERSION_STRING = VERSION.join(".")
